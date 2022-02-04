f0124c14ec4ee2481d1ed47592e715bfaea3d949
grexn <nox-rox@web.de>
2022-02-04 02:16:34 +0100

    Add additional csv file export

--- fetch_arc69/fetch_arc69.py
+++ fetch_arc69/fetch_arc69.py
@@ -7,6 +7,8 @@ Created on Wed Feb 02 21:21:39 2022
 """
 import base64
 import os
+import json
+import csv
 from algosdk.v2client import indexer
 
 
@@ -24,22 +26,55 @@ HEADERS = {'User-Agent': 'py-algorand-sdk'}
 myindexer = indexer.IndexerClient(indexer_token="", headers=HEADERS, indexer_address=INDEXER_ADDRESS)
 
 
-def writeMetaDataToFiles(indexer, public_key):
+def write_meta_data_to_files(indexer, public_key):
     created_assets = indexer.account_info(address=public_key)['account']['created-assets']
+    data = []
     for asset in created_assets:
         asset_id = asset['index']
         is_deleted = asset['deleted']
         last_config_tnx = myindexer.search_asset_transactions(asset_id,txn_type='acfg')['transactions'][-1]
         if 'note' in last_config_tnx and not is_deleted:
+            print(f"ASA ID {asset_id}: metadata found - adding.")
             file_path = f"{OUTPUT_PATH}{asset_id}.json"
-            checkAndCreatePath(file_path)
+            check_and_create_path(file_path)
             with open(file_path, "w", encoding='utf-8') as json_file:
-                json_file.write(base64.b64decode(last_config_tnx['note']).decode('utf-8'))
+                json_string = base64.b64decode(last_config_tnx['note']).decode('utf-8')
+                json_file.write(json_string)
+                json_data_as_dict = json.loads(json_string)
+                data.append((asset_id, json_data_as_dict))
+        else:
+            print(f"ASA ID {asset_id}: no metadata found.")
+
+    write_csv_file(data)
 
 
-def checkAndCreatePath(file_path):
+def write_csv_file(data):
+    sortedData = sorted(data)
+
+    converted_data = []
+    attribute_keys = []
+
+    for asset_id, asset in sortedData:
+        new_asset = {
+            'description': asset['description']
+        }
+        for attribute in asset['properties'].keys():
+            new_asset[attribute] = asset['properties'][attribute]
+        converted_data.append(new_asset)
+        for attribute in new_asset:
+            if attribute not in attribute_keys:
+                attribute_keys.append(attribute)
+
+    csv_file_path = f"{OUTPUT_PATH}metadata.csv"
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as f: 
+        wr = csv.DictWriter(f, fieldnames = attribute_keys) 
+        wr.writeheader()
+        wr.writerows(converted_data) 
+
+
+def check_and_create_path(file_path):
     if not os.path.exists(os.path.dirname(file_path)):
         os.makedirs(os.path.dirname(file_path))
 
 
-writeMetaDataToFiles(myindexer, PUBLIC_KEY)
+write_meta_data_to_files(myindexer, PUBLIC_KEY)
