13de3369d045cc27cc21cef91f567d319c4de0cd
grexn <nox-rox@web.de>
2022-02-06 13:50:09 +0100

    Revert "Rever batch_mint script"
    This reverts commit b32adc7613b3cacfb6b78ff7faa2240f3a30486b.

--- batch_mint_arc69/mint_arc69.py
+++ batch_mint_arc69/mint_arc69.py
@@ -14,22 +14,9 @@ from natsort import natsorted
 import requests
 
 
-def mint_asset (n, unit_name, asset_name, mnemonic1, image_path, meta_path, meta_type, api_key, api_secret, external_url, description, testnet=True):
-    imgs = natsorted(glob.glob(os.path.join(image_path, "*.png")))
-    
-    files = [('file', (str(n)+".png", open(imgs[n], "rb"))),]
-    
-    headers = {        
-        'pinata_api_key': api_key,
-        'pinata_secret_api_key': api_secret    
-    }   
-    
-    ipfs_url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
-    
-    response: requests.Response = requests.post(url=ipfs_url, files=files, headers=headers)
-    meta = response.json()
-    
-    ipfs_cid = meta['IpfsHash']
+def mint_asset (n, unit_name, asset_name, unit_name_number_digits, asset_name_number_digits, mnemonic1, image_path, meta_path, meta_type, api_key, api_secret, external_url, description, use_csv_asset_name, use_csv_cid, use_csv_description, testnet=True):
+    if not use_csv_cid:
+        ipfs_cid = get_cid_from_pinata(n, image_path, api_key, api_secret)
 
     if (meta_type=="csv"):
         d = pd.read_csv(meta_path)    
@@ -37,36 +24,40 @@ def mint_asset (n, unit_name, asset_name, mnemonic1, image_path, meta_path, meta
         items = items[items != "None"]
         items = items.dropna()
         items = items.apply(str)
-        attributes = items.to_json()
+        if use_csv_asset_name:
+            csv_asset_name = items.pop('name')
+        if use_csv_cid:
+            csv_ipfs_url = items.pop('cid')
+        if use_csv_description:
+            description = items.pop('description')
+        attributes =  items.to_dict()
     
     elif (meta_type=="JonBecker"):
         d = pd.read_json(meta_path)    
         d.drop('tokenId', axis=1, inplace=True)
         items = d.iloc[n]
         items = items[items != "None"]
-        attributes = items.to_json()
+        attributes = items.to_dict()
     
     elif (meta_type=="HashLips"):
         d = pd.read_json(meta_path)
         l = d['attributes'][n]
         records = pd.DataFrame.from_records(l).set_index('trait_type')
-        attributes = records.iloc[:,0].to_json()       
-    
-    if (external_url==""):
-        if (description==""):
-            meta_data = '{"standard":"arc69", "properties":' + attributes + '}' 
-        else:
-            meta_data = '{"standard":"arc69", "description":"' + description + '","properties":' + attributes + '}' 
-    else:
-        if (description==""):
-            meta_data = '{"standard":"arc69"' + ',"external_url":"' + external_url + '","properties":'  + attributes + '}' 
-            meta_data = meta_data.replace("'", '"')                
-        else:
-            meta_data = '{"standard":"arc69"' + ',"external_url":"' + external_url + '","description":"' + description + '","properties":'  + attributes + '}' 
-            meta_data = meta_data.replace("'", '"')    
-    
-    print(meta_data)
-        
+        attributes = records.iloc[:,0].to_dict()
+
+
+    meta_data = {
+        "standard": "arc69",
+        "description": description,
+        "external_url": external_url,
+        "properties": attributes
+    }
+
+    # Remove keys with empty values
+    meta_data = { key: value for key, value in meta_data.items() if value != '' }
+
+    meta_data_json = json.dumps(meta_data)
+            
     # an accounts dict.
     accounts = {}
     counter = 1
@@ -109,7 +100,7 @@ def mint_asset (n, unit_name, asset_name, mnemonic1, image_path, meta_path, meta
         # response = myindexer.accounts(asset_id = assetid)
         # then use 'account_info['created-assets'][0] to get info on the created asset
         account_info = algodclient.account_info(account)
-        idx = 0;
+        idx = 0
         for my_account_info in account_info['created-assets']:
             scrutinized_asset = account_info['created-assets'][idx]
             idx = idx + 1       
@@ -127,25 +118,29 @@ def mint_asset (n, unit_name, asset_name, mnemonic1, image_path, meta_path, meta
     # comment these two lines if you want to use suggested params
     params.fee = 1000
     params.flat_fee = True
-    
+    asset_number = str(n+1)
+    asset_name = csv_asset_name if use_csv_asset_name else asset_name + asset_number.zfill(asset_name_number_digits)
+    url = csv_ipfs_url if use_csv_cid else f"ipfs://${ipfs_cid}"
 
     txn = AssetConfigTxn(
         sender=accounts[1]['pk'],
         sp=params,
         total= 1,
         default_frozen=False,
-        unit_name=unit_name +str(n+1),
-        asset_name=asset_name + str(n+1), 
+        unit_name=unit_name + asset_number.zfill(unit_name_number_digits),
+        asset_name=asset_name, 
         manager=accounts[1]['pk'],
         reserve=accounts[1]['pk'],
         freeze=None,
         clawback=None,
         strict_empty_address_check=False,
-        url="ipfs://" +ipfs_cid,
+        url=url,
         metadata_hash= "", 
-        note = meta_data.encode(),
+        note = meta_data_json.encode(),
 
         decimals=0)
+
+
     # Sign with secret key of creator
     stxn = txn.sign(accounts[1]['sk'])
     
@@ -170,3 +165,20 @@ def mint_asset (n, unit_name, asset_name, mnemonic1, image_path, meta_path, meta
         print_created_asset(algod_client, accounts[1]['pk'], asset_id)
     except Exception as e:
         print(e)
+
+def get_cid_from_pinata(n, image_path, api_key, api_secret):
+    imgs = natsorted(glob.glob(os.path.join(image_path, "*.png")))
+        
+    files = [('file', (str(n)+".png", open(imgs[n], "rb"))),]
+        
+    headers = {        
+            'pinata_api_key': api_key,
+            'pinata_secret_api_key': api_secret    
+        }   
+        
+    ipfs_url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+        
+    response: requests.Response = requests.post(url=ipfs_url, files=files, headers=headers)
+    meta = response.json()
+        
+    return meta['IpfsHash']
