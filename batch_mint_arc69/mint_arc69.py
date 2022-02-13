1ea059477db9f83360ad8e7a3a3a5c91caae1699
grexn <nox-rox@web.de>
2022-02-13 15:40:47 +0100

    Refactor batch update

--- batch_mint_arc69/mint_arc69.py
+++ batch_mint_arc69/mint_arc69.py
@@ -15,6 +15,7 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
 from lib.settings import Settings
+from lib.algod_helper import wait_for_confirmation, print_created_asset
 
 
 def mint_asset(n, settings: Settings):
@@ -149,34 +150,3 @@ def get_cid_from_pinata(n, image_path, api_key, api_secret):
     meta = response.json()
         
     return meta['IpfsHash']
-
-
-def wait_for_confirmation(client, txid):
-    """
-    Utility function to wait until the transaction is
-    confirmed before proceeding.
-    """
-    last_round = client.status().get('last-round')
-    txinfo = client.pending_transaction_info(txid)
-    while not (txinfo.get('confirmed-round') and txinfo.get('confirmed-round') > 0):
-        print("Waiting for confirmation")
-        last_round += 1
-        client.status_after_block(last_round)
-        txinfo = client.pending_transaction_info(txid)
-    print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
-    return txinfo
-
-#   Utility function used to print created asset for account and assetid
-def print_created_asset(algodclient, account, assetid):    
-    # note: if you have an indexer instance available it is easier to just use this
-    # response = myindexer.accounts(asset_id = assetid)
-    # then use 'account_info['created-assets'][0] to get info on the created asset
-    account_info = algodclient.account_info(account)
-    idx = 0
-    for my_account_info in account_info['created-assets']:
-        scrutinized_asset = account_info['created-assets'][idx]
-        idx = idx + 1       
-        if (scrutinized_asset['index'] == assetid):
-            print("Asset ID: {}".format(scrutinized_asset['index']))
-            print(json.dumps(my_account_info['params'], indent=4))
-            break
---
+++ lib/algod_helper.py
@@ -0,0 +1,43 @@
+import json
+
+def wait_for_confirmation(client, txid):
+    """
+    Utility function to wait until the transaction is
+    confirmed before proceeding.
+    """
+    last_round = client.status().get('last-round')
+    txinfo = client.pending_transaction_info(txid)
+    while not (txinfo.get('confirmed-round') and txinfo.get('confirmed-round') > 0):
+        print("Waiting for confirmation")
+        last_round += 1
+        client.status_after_block(last_round)
+        txinfo = client.pending_transaction_info(txid)
+    print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
+    return txinfo
+
+#   Utility function used to print created asset for account and assetid
+def print_created_asset(algodclient, account, assetid):    
+    # note: if you have an indexer instance available it is easier to just use this
+    # response = myindexer.accounts(asset_id = assetid)
+    # then use 'account_info['created-assets'][0] to get info on the created asset
+    account_info = algodclient.account_info(account)
+    idx = 0
+    for my_account_info in account_info['created-assets']:
+        scrutinized_asset = account_info['created-assets'][idx]
+        idx = idx + 1       
+        if (scrutinized_asset['index'] == assetid):
+            print("Asset ID: {}".format(scrutinized_asset['index']))
+            print(json.dumps(my_account_info['params'], indent=4))
+            break
+
+#   Utility function used to print asset holding for account and assetid
+def print_asset_holding(algodclient, account, assetid):
+    account_info = algodclient.account_info(account)
+    idx = 0
+    for my_account_info in account_info['assets']:
+        scrutinized_asset = account_info['assets'][idx]
+        idx = idx + 1        
+        if (scrutinized_asset['asset-id'] == assetid):
+            print("Asset ID: {}".format(scrutinized_asset['asset-id']))
+            print(json.dumps(scrutinized_asset, indent=4))
+            break
